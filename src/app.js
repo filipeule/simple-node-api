535f41d270db5ea887c7caa1c4352e81cbd29ae9
Filipe Ule <filipe_ule@hotmail.com>
2026-03-09 22:15:27 -0300

    feat(api): adiciona handler para tratamento de rotas não encontradas na api

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
 import express from 'express'
-import pino from "pino-http"
-import swaggerUi from "swagger-ui-express"
-import swaggerSpec from "./docs/swagger.js"
+import pino from 'pino-http'
+import swaggerUi from 'swagger-ui-express'
+import swaggerSpec from './docs/swagger.js'
 
 import orderRouter from './routers/orderRouter.js'
 import userRouter from './routers/userRouter.js'
 import authRouter from './routers/authRouter.js'
 import { authMiddleware } from './middleware/authMiddleware.js'
+import { notFound } from './middleware/notFound.js'
 
 const app = express()
 
 app.use(pino())
 app.use(express.json())
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.use('/orders', authMiddleware, orderRouter)
 app.use('/users', userRouter)
 app.use('/login', authRouter)
+
+app.use(notFound)
 
 export default app
